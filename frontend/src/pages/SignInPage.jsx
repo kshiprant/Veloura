f67422f96c7ca41268c41d0f8ad5a184f941ca92
kshiprant <kshiprant19@gmail.com>
2026-03-17 14:11:18 +0530

    Create SignInPage.jsx

---
+++ frontend/src/pages/SignInPage.jsx
@@ -0,0 +1,45 @@
+import { useState } from "react";
+import axios from "axios";
+
+export default function SignInPage() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await axios.post(
+        import.meta.env.VITE_API_URL + "/auth/login",
+        { email, password }
+      );
+
+      localStorage.setItem("token", res.data.token);
+      alert("Login successful");
+    } catch (err) {
+      alert("Login failed");
+    }
+  };
+
+  return (
+    <div style={{ padding: 20 }}>
+      <h2>Sign In</h2>
+      <form onSubmit={handleLogin}>
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        /><br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        /><br /><br />
+
+        <button type="submit">Login</button>
+      </form>
+    </div>
+  );
+}
