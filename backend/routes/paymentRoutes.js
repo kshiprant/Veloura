44eeaf261fd85735c361fbd5fd6882fcdfa2923f
kshiprant <kshiprant19@gmail.com>
2026-03-18 22:18:02 +0530

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,17 +1,9 @@
 import express from 'express';
 import auth from '../src/middleware/auth.js';
-import { createOrder } from '../controllers/paymentController.js';
+import { createOrder } from '../src/controllers/paymentController.js';
 
 const router = express.Router();
 
-// Create Razorpay order
-router.post('/create-order', auth, async (req, res, next) => {
-  try {
-    await createOrder(req, res);
-  } catch (error) {
-    console.error('create-order route error:', error);
-    next(error);
-  }
-});
+router.post('/create-order', auth, createOrder);
 
 export default router;
