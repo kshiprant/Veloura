199ddd08a2ad573e5d8314d6eef2cf46bfd96bb6
kshiprant <kshiprant19@gmail.com>
2026-03-18 21:03:34 +0530

    Update paymentRoutes.js

--- backend/routes/paymentRoutes.js
+++ backend/routes/paymentRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import auth from '../middleware/auth.js';
+import auth from '../src/middleware/auth.js';
 import { createOrder } from '../controllers/paymentController.js';
 
 const router = express.Router();
